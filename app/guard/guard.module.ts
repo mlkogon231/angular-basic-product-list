cc9c78b2af4ad332ac3ed1a14a51853ab3fd8719
Mark Kogon <mlkogon231@gmail.com>
2018-07-20 15:33:44 -0700

    first commit

---
+++ app/guard/guard.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GuardComponent } from './guard/guard.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GuardComponent]
+})
+export class GuardModule { }
